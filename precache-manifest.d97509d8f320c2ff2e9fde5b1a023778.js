2fd8450871a2817aa9fc06e6e9c0eb9877a21031
Ross Gravitte <Rbgravitte@gmail.com>
2019-01-22 16:56:38 -0500

    Updates

---
+++ precache-manifest.d97509d8f320c2ff2e9fde5b1a023778.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1acf46ee9182e587faf8",
+    "url": "/React_Project_SPA/static/css/main.70ba5b83.chunk.css"
+  },
+  {
+    "revision": "1acf46ee9182e587faf8",
+    "url": "/React_Project_SPA/static/js/main.1acf46ee.chunk.js"
+  },
+  {
+    "revision": "8f390d2f61b36da8ebad",
+    "url": "/React_Project_SPA/static/css/1.ff5c4854.chunk.css"
+  },
+  {
+    "revision": "8f390d2f61b36da8ebad",
+    "url": "/React_Project_SPA/static/js/1.8f390d2f.chunk.js"
+  },
+  {
+    "revision": "832c6c6fa98e4698a5a8",
+    "url": "/React_Project_SPA/static/js/runtime~main.832c6c6f.js"
+  },
+  {
+    "revision": "57fc92e30f4b20c5b66b85e3b59e35ba",
+    "url": "/React_Project_SPA/index.html"
+  }
+];
